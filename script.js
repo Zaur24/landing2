47af85020639c5fc035c1b0721e59ba5597286ea
zaur <zaur@yan.ru>
2024-02-09 11:17:35 +0300

    ros

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('Hello World')
+
+
+let signInBtn = document.getElementById('sign-in-btn')
+let modal = document.querySelector('.modal')
+signInBtn.onclick = function (event) {
+    event.preventDefault()
+    modal.classList.add('show')
+}
+
+
+
+modal.onclick = function(event) {
+    // console.log(event.target)
+    if (event.target.classList.contains('modal')) {
+        modal.classList.remove('show')
+    }
+}
+
+let closeModalBtn = document.querySelector('.close-modal-btn')
+closeModalBtn.onclick = function (event) {
+    modal.classList.remove('show')
+}
+
+
+let toggleThemeBtn = document.getElementById('toggle-theme')
+toggleThemeBtn.onclick = function(event) {
+    document.body.classList.toggle('dark')
+}
